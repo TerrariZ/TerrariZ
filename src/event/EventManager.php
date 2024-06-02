421376caaa42358fe679a6587a4ebe1c01c89705
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-06-02 12:45:37 +1000

    Create EventManager.php

---
+++ src/event/EventManager.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TerrariZ;
+
+use TerrariZ;
+use TerrariZ\event\ServerStartEvent;
+
+final class EventManager {
+private function stopServer() bool {
+
+  return true;
+}
+
+}
+
+?>
