f9912075549429e344b2ebf8ef0c1a38cc7b0f2b
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-06-03 14:26:01 +1000

    Add files via upload

---
+++ src/EventManager.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TerrariZ;
+use TerrariZ\Player;
+final class EventManager() {
+// PlayerJoin Event will aim to utilise this function to Cancel a Player from joining whether they have an outdated version of terraria or if they are even banned or not added to a whitelist.
+public function cancelJoin(string $str, Player $player , Array $args):bool ;
+//TODO: Make Kick Function
+$kickMessage = $str;
+
+
+
+
+}
+
+
+
+?>
---
+++ src/PlayerJoinEvent.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace TerrariZ;
+use TerrariZ\event\Server\ServerStartEvent;
+use TerrariZ\event\Server\ServerStopEvent;
+use TerrariZ\event\EventManager;
+use TerrariZ\player\Player;
+use TerrariZ\utils\Logger;
+
+final class PlayerJoinEvent {
+public const $player = Player;
+public const $logger = Logger;
+
+
+public function isServerWhitelisted() : bool {
+
+return $whitelist;
+
+}
+
+public function isPlayerOnWhitelist(): bool {
+if ($isServerWhitelisted() === true){
+//TODO:: CHECK IF PLAYER IS ON WHITELIST;
+return $isPlayerOnWhitelist;
+
+}
+
+public function isServerOn(ServerStartEvent $start , ServerStopEvent $stop, EventManager $event) : bool {
+	$stopped = $stop->isStopped();
+	$started = $start->isStarted();
+	
+	if ($stopped === false and $started === true){
+		
+		return true;
+		
+		
+	}
+	
+		if ($stopped === true and $started === true){
+		$logger(error,"Server is Stated on and off?");
+		throw new Exception("Server is Stated off and On?");
+		
+	}
+	
+		if ($stopped === true and $started === false){
+		
+		return false;
+		$event->cancelJoin($player->getName() ,"Server is Offline");
+		
+	}
+		if ($stopped === false and $started === false){
+		$logger(error,"Server is Stated off but stopped is false?");
+		throw new Exception("Server is Stated off but stopped is false?");
+		
+		
+	}
+	
+	
+	
+}
+
+}
+}
+
+?>
