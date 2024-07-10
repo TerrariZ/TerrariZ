36cf2bc145daef3edf4e73e10c2340c3575101f7
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-07-10 19:04:05 +1000

    Update AcknowledgementPacket.php

--- src/internal/Packets/AcknowledgementPacket.php
+++ src/internal/Packets/AcknowledgementPacket.php
@@ -7,7 +7,7 @@ namespace TerrariZ\AcknowledgementPacket;
 abstract class AcknowledgementPacket {
 
 
-public const $id = "0x003"
+public const $id = "0x0006"
 
 public function acknowledge() : bool {
 return $id;
