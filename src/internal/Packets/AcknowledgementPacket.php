aa1527f90a6b32fb8f9197c250ff29894ab81fef
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-07-10 19:03:05 +1000

    Acknowledge Packet

---
+++ src/internal/Packets/AcknowledgementPacket.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace TerrariZ\AcknowledgementPacket;
+
+
+abstract class AcknowledgementPacket {
+
+
+public const $id = "0x003"
+
+public function acknowledge() : bool {
+return $id
+}
+}
+
+
+?>
