ddfa0a1e42301c14be0a6843d4d79eb6c1faaeec
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-07-18 19:43:20 +1000

    Update src

---
+++ src/player/Player.php
@@ -0,0 +1,25 @@
+<?php
+namespace TerrariZ\Player;
+
+use TerrariZ\utils\Logger;
+use TerrariZ\utils\TranslateTable;
+
+
+
+class Player {
+
+/*
+
+Player Class has alot to do. coming soon 
+
+*/
+
+public function isWhitelisted(): bool {
+$plrdata = $this->getResourceFolder()->get(whitelist.json)
+
+
+}
+
+
+}
+?>
