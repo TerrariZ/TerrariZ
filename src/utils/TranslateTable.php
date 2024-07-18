2977159757d1cf97d7f51d445fc6838ecaecd5c0
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-07-18 19:28:57 +1000

    Update utils

---
+++ src/utils/TranslateTable.php
@@ -0,0 +1,23 @@
+<?php
+namespace TerrariZ\TranslateTable;
+
+
+final class TranslateTable {
+public const Table =[
+
+[ServerOffline = "Server is Offline"],
+[ServerWhitelisted = "Server is Whitelisted"],
+[ServerOutOfDate = "Server is Out of Date."],
+[ClientOutOfDate = "Client is Out of Date."],
+[ClientIPBanned = "Access Denied to Your IP Address."],
+[ClientTempBanned = "You Have Been Temporarily Banned."],
+[ClientBoot = "Kicked from the Server."],
+[ServerStarting = "Server is Still Starting."],
+[ServerStopping = "Server is Stopping."],
+[ServerCrashed = "Server has Crashed."]
+
+];
+
+}
+
+?>
